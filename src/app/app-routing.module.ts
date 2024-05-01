615075f3326fe518aa6d7ffd7977d2510cdece18
RubyEsme <schoolwork.rlc@gmail.com>
2024-05-01 17:39:24 -0600

    Routing, Welcome, Ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Bienvenida', component: FormularioComponent},
+  //{path: 'About You', component: AboutUser},
+  {path: '**', redirectTo: 'Bienvenida'}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
